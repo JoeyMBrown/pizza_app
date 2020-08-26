ad5b08336348db3119f7f1d3dcd5086316ef5519
JoeyMBrown <Joey.ma.brown@gmail.com>
2020-08-25 22:44:41 -0400

    Added variable saving to local storage.

--- logic.js
+++ logic.js
@@ -1,11 +1,85 @@
 window.onload = init;
 
+var shiftSummary = {
+
+    totalDeliveries: 0,
+    outOfTownOrders: 0,
+    inTownOrders: 0,
+    cashTipTotal: 0,
+    cardTipTotal: 0,
+    tipTotal: 0,
+    startingMiles: 0,
+    endingMiles: 0,
+    totalMiles: 0
+
+}
+
 function init() {
+
+    //localStorage.clear();
+
+if(!localStorage.getItem('shiftSummary')) {
+        localStorage.setItem('shiftSummary', JSON.stringify({
+            
+            totalDeliveries: 0,
+            outOfTownOrders: 0,
+            inTownOrders: 0,
+            cashTipTotal: 0,
+            cardTipTotal: 0,
+            tipTotal: 0,
+            startingMiles: 0,
+            endingMiles: 0,
+            totalMiles: 0
+
+        })
+    )
+}  else if (localStorage.getItem('shiftSummary')) {
+    var localShiftSummary = localStorage.getItem('shiftSummary');
+    var parsedShiftSummary = JSON.parse(localShiftSummary);
+
+    var cardTipTotalElement = document.getElementById("cardTipTotal");
+    var tipTotalElement = document.getElementById("tipTotal");
+    var cashTipTotalElement = document.getElementById("cashTipTotal");
+    var totalDeliveriesElement = document.getElementById("totalDeliveries");
+    var outOfTownOrdersElement = document.getElementById("outOfTownOrders");
+    var inTownOrdersElement = document.getElementById("inTownOrders");
+    var startingMilesElement = document.getElementById("startingMiles");
+    var endingMilesElement = document.getElementById("endingMiles");
+    var totalMilesElement = document.getElementById("totalMiles");
+
+    shiftSummary.totalDeliveries = parsedShiftSummary.totalDeliveries
+    shiftSummary.outOfTownOrders = parsedShiftSummary.outOfTownOrders
+    shiftSummary.inTownOrders = parsedShiftSummary.inTownOrders
+    shiftSummary.cashTipTotal = parsedShiftSummary.cashTipTotal
+    shiftSummary.cardTipTotal = parsedShiftSummary.cardTipTotal
+    shiftSummary.tipTotal = parsedShiftSummary.tipTotal
+    shiftSummary.startingMiles = parsedShiftSummary.startingMiles
+    shiftSummary.endingMiles = parsedShiftSummary.endingMiles
+    shiftSummary.totalMiles = parsedShiftSummary.totalMiles
+
+    //console.log(shiftSummary.cardTipTotal);
+
+    cardTipTotalElement.innerHTML = parsedShiftSummary.cardTipTotal
+    tipTotalElement.innerHTML = parsedShiftSummary.tipTotal
+    cashTipTotalElement.innerHTML = parsedShiftSummary.cashTipTotal
+    totalDeliveriesElement.innerHTML = parsedShiftSummary.totalDeliveries
+    outOfTownOrdersElement.innerHTML = parsedShiftSummary.outOfTownOrders
+    inTownOrdersElement.innerHTML = parsedShiftSummary.inTownOrders
+    startingMilesElement.innerHTML = parsedShiftSummary.startingMiles
+    endingMilesElement.innerHTML = parsedShiftSummary.endingMiles
+    totalMilesElement.innerHTML = parsedShiftSummary.totalMiles
+
+
+    console.log(shiftSummary);
+    //console.log(parsedShiftSummary.cardTipTotal);
+}
+
     var newOrder = document.getElementById("newOrder");
     var inTown = document.getElementById("inTown");
     var outOfTown = document.getElementById("outOfTown");
     var startShift = document.getElementById("startShift");
     var endShift = document.getElementById("endShift");
+    var resetShift = document.getElementById("resetShift");
     
 
     function onClickHandler(button) {
@@ -17,6 +91,7 @@ function init() {
     outOfTown.onclick = onClickHandler("outOfTown");
     startShift.onclick = onClickHandler("startShift");
     endShift.onclick = onClickHandler("endShift");
+    resetShift.onclick = onClickHandler("resetShift");
 
 }
 
@@ -35,8 +110,13 @@ function cashTip() {
     }
 
     else {
-        cashTipTotalElement.innerHTML = cashTipTotal += cashTipVal;
-        tipTotalElement.innerHTML = tipTotal += cashTipVal;
+        cashTipTotalElement.innerHTML = shiftSummary.cashTipTotal + cashTipVal;
+        tipTotalElement.innerHTML = shiftSummary.tipTotal + cashTipVal;
+
+        shiftSummary.cashTipTotal = shiftSummary.cashTipTotal + cashTipVal
+        shiftSummary.tipTotal = shiftSummary.tipTotal + cashTipVal
+
+        localStorage.setItem('shiftSummary', JSON.stringify(shiftSummary));
 
         document.getElementById("cashTipInput").value = "";
     }
@@ -44,23 +124,30 @@ function cashTip() {
 
 function cardTip() {
     var cardTipTotalElement = document.getElementById("cardTipTotal");
-    var cardTipTotal = parseFloat(cardTipTotalElement.innerHTML);
 
     var inputVal = document.getElementById("cardTipInput").value;
     var cardTipVal = parseFloat(inputVal);
 
     var tipTotalElement = document.getElementById("tipTotal");
-    var tipTotal = parseFloat(tipTotalElement.innerHTML);
 
     if(inputVal.length < 1) {
         alert("Please enter a tip amount")
     }
 
     else {
-        cardTipTotalElement.innerHTML = cardTipTotal += cardTipVal;
-        tipTotalElement.innerHTML = tipTotal += cardTipVal;
+        cardTipTotalElement.innerHTML = shiftSummary.cardTipTotal + cardTipVal;
+        tipTotalElement.innerHTML = shiftSummary.tipTotal + cardTipVal;
+
+        shiftSummary.cardTipTotal = shiftSummary.cardTipTotal + cardTipVal
+        shiftSummary.tipTotal = shiftSummary.tipTotal + cardTipVal
+
+        localStorage.setItem('shiftSummary', JSON.stringify(shiftSummary));
 
         document.getElementById("cardTipInput").value = "";
+        //console.log(shiftSummary);
+        //console.log(cardTipVal);
+        //console.log(shiftSummary.cardTipTotal);
+        // Above I'm assigning the shift value.  I need to add it to the cardtipval, and assign it seperately!
     }
 }
 
@@ -68,7 +155,6 @@ var display = {
 
     handleButtons: function(button) {
        var totalDeliveriesElement = document.getElementById("totalDeliveries");
-       var totalDeliveries = parseFloat(totalDeliveriesElement.innerHTML);
 
        var inTownOrdersElement = document.getElementById("inTownOrders");
        var inTownOrders = parseFloat(inTownOrdersElement.innerHTML);
@@ -86,24 +172,47 @@ var display = {
        var totalMiles = parseFloat(totalMilesElement.innerHTML);
    
        if (button == "newOrder") {
-            totalDeliveriesElement.innerHTML = totalDeliveries += 1;
+            totalDeliveriesElement.innerHTML = shiftSummary.totalDeliveries += 1;
        } else if (button == "inTown") {
-            inTownOrdersElement.innerHTML = inTownOrders += 1;
+            inTownOrdersElement.innerHTML = shiftSummary.inTownOrders += 1;
        } else if (button == "outOfTown") {
-            outOfTownOrdersElement.innerHTML = outOfTownOrders += 1;
+            outOfTownOrdersElement.innerHTML = shiftSummary.outOfTownOrders += 1;
        } else if(button == "startShift") {
-            var inputValue = prompt()
+            var inputValue = prompt("Please enter your cars mile range at the START of your shift.")
             var inputValueInt = parseFloat(inputValue);
 
-            startingMilesElement.innerHTML = startingMiles += inputValueInt;
+            shiftSummary.startingMiles = inputValueInt;
+
+            startingMilesElement.innerHTML = inputValueInt;
        } else if(button == "endShift") {
-           var inputValue = prompt();
+           var inputValue = prompt("Please enter your cars mile range at the END of your shift.");
            var inputValueInt = parseFloat(inputValue);
 
-           endingMilesElement.innerHTML = endingMiles += inputValueInt;
-           totalMilesElement.innerHTML = startingMiles -= endingMiles;
+           shiftSummary.endingMiles = inputValueInt;
+           shiftSummary.totalMiles = shiftSummary.startingMiles - shiftSummary.endingMiles;
+
+           endingMilesElement.innerHTML = inputValueInt;
+           totalMilesElement.innerHTML = shiftSummary.startingMiles - shiftSummary.endingMiles;
        } else {
            console.log("why is this being logged");
      }
+     localStorage.setItem('shiftSummary', JSON.stringify(shiftSummary));
+
+        if(button == "resetShift") {
+            if(confirm("WARNING: Clicking OKAY will wipe all shift information back to 0!")) {
+                localStorage.clear();
+                location.reload();
+        } else {
+            console.log("weewees");
+        }
+    } else {
+        console.log("weewees")
+    }
    }
 }
+
+            //localStorage.clear()
+            //localStorage.setItem('myCat', 'Tom');
+
+            //var cat = localStorage.getItem('myCat');
+            //alert(cat);
